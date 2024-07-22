955c8e0ae8e7403147b21c576a1c21d733e987d2
Matheus <mathgunz@gmail.com>
2024-07-22 16:06:58 -0300

    adicionando keycloak

--- src/main/java/com/temperaturamusical/controllers/TemperaturaMusicaController.java
+++ src/main/java/com/temperaturamusical/controllers/TemperaturaMusicaController.java
@@ -1,5 +1,6 @@
 package com.temperaturamusical.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,4 +38,18 @@ public class TemperaturaMusicaController {
         return temperaturaMusica;
     }
 
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello");
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> sayHelloToAdmin() {
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<String> sayHelloToUser() {
+        return ResponseEntity.ok("Hello User");
+    }
 }
